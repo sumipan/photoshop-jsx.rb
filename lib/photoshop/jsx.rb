9624d06f1f67a723fb2d662ae3bb641c23bf9b2a
takashi nagayasu <regist@g-onion.org>
2015-11-16 11:35:32 +0900

    Add wait

--- lib/photoshop/jsx.rb
+++ lib/photoshop/jsx.rb
@@ -4,10 +4,10 @@ module Photoshop
   module Jsx
     include FileUtils
 
-    def self.proc(psd, jsx)
+    def self.proc(psd, jsx, waitTime=0)
       applescript = File.expand_path('../../../bin/PhotoshopJsx.scpt', __FILE__)
 
-      system("/usr/bin/osascript #{applescript} '#{psd}' '#{jsx}'")
+      system("/usr/bin/osascript #{applescript} '#{psd}' '#{jsx}' #{waitTime}")
     end
   end
 end
